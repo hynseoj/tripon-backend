8deb04d7ff1863cf375c46bacfd3591b9987559f
minju <cash0522@hanyang.ac.kr>
2025-05-11 04:49:18 +0900

    test: 여행계획 테스트 코드 수정

--- src/test/java/com/ssafy/tripon/plan/application/PlanServiceTest.java
+++ src/test/java/com/ssafy/tripon/plan/application/PlanServiceTest.java
@@ -20,6 +20,7 @@ class PlanServiceTest {
 
     @Autowired
     private PlanService planService;
+    private PlanServiceResponse response;
 
     @DisplayName("계획을 저장할 수 있다")
     @Test
@@ -30,16 +31,16 @@ class PlanServiceTest {
         );
 
         // when
-        Integer savedId = planService.savePlan(command);
+        Integer planId = planService.savePlan(command);
 
         // then
-        PlanServiceResponse response = planService.findPlanById(savedId);
+        response = planService.findPlanById(planId);
         assertThat(response.title()).isEqualTo("제주 여행");
     }
 
     @DisplayName("사용자 ID로 계획 목록을 조회할 수 있다")
     @Test
-    void canGetPlanListByMemberId() {
+    void canFindAllPlanByMemberId() {
         // given
         String memberId = "admin@ssafy.com";
         planService.savePlan(new PlanSaveCommand(memberId, "A 여행", LocalDate.now(), LocalDate.now().plusDays(1), ""));
@@ -59,44 +60,44 @@ class PlanServiceTest {
         PlanSaveCommand command = new PlanSaveCommand(
                 "admin@ssafy.com", "부산 여행", LocalDate.now(), LocalDate.now().plusDays(3), "부산 메모"
         );
-        Integer savedId = planService.savePlan(command);
+        Integer planId = planService.savePlan(command);
 
         // when
-        PlanServiceResponse plan = planService.findPlanById(savedId);
+        response = planService.findPlanById(planId);
 
         // then
-        assertThat(plan.memo()).isEqualTo("부산 메모");
+        assertThat(response.memo()).isEqualTo("부산 메모");
     }
 
     @DisplayName("계획을 수정할 수 있다")
     @Test
     void canUpdatePlan() {
         // given
-        Integer savedId = planService.savePlan(new PlanSaveCommand("admin@ssafy.com", "오래된 여행", LocalDate.now(), LocalDate.now().plusDays(1), ""));
+        Integer planId = planService.savePlan(new PlanSaveCommand("admin@ssafy.com", "오래된 여행", LocalDate.now(), LocalDate.now().plusDays(1), ""));
         PlanUpdateCommand updateCommand = new PlanUpdateCommand(
-                savedId, "업데이트된 여행", LocalDate.now(), LocalDate.now().plusDays(4), "업데이트 메모"
+        		planId, "업데이트된 여행", LocalDate.now(), LocalDate.now().plusDays(4), "업데이트 메모"
         );
 
         // when
         planService.updatePlanById(updateCommand);
 
         // then
-        PlanServiceResponse updated = planService.findPlanById(savedId);
-        assertThat(updated.title()).isEqualTo("업데이트된 여행");
-        assertThat(updated.memo()).isEqualTo("업데이트 메모");
+        response = planService.findPlanById(planId);
+        assertThat(response.title()).isEqualTo("업데이트된 여행");
+        assertThat(response.memo()).isEqualTo("업데이트 메모");
     }
 
     @DisplayName("계획을 삭제할 수 있다")
     @Test
     void canDeletePlan() {
         // given
-        Integer savedId = planService.savePlan(new PlanSaveCommand("admin@ssafy.com", "삭제할 여행", LocalDate.now(), LocalDate.now().plusDays(1), ""));
+        Integer planId = planService.savePlan(new PlanSaveCommand("admin@ssafy.com", "삭제할 여행", LocalDate.now(), LocalDate.now().plusDays(1), ""));
 
         // when
-        planService.deletePlanById(savedId);
+        planService.deletePlanById(planId);
 
         // then
         List<PlanServiceResponse> plans = planService.findAllPlanByMemberId("admin@ssafy.com");
-        assertThat(plans).noneMatch(plan -> plan.planId().equals(savedId));
+        assertThat(plans).noneMatch(plan -> plan.planId().equals(planId));
     }
 }

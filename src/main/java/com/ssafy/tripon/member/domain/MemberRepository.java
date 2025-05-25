25d3bb298d5f0fd5c480b355fe8ba59a154429aa
minju <cash0522@hanyang.ac.kr>
2025-05-25 18:56:40 +0900

    feat: 댓글 조회 최적화를 위한 이메일 기반 사용자 이름 조회 기능 추가

--- src/main/java/com/ssafy/tripon/member/domain/MemberRepository.java
+++ src/main/java/com/ssafy/tripon/member/domain/MemberRepository.java
@@ -2,6 +2,7 @@ package com.ssafy.tripon.member.domain;
 
 import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 @Mapper
 public interface MemberRepository {
@@ -12,4 +13,7 @@ public interface MemberRepository {
     int update(Member member);
     void deleteByEmail(String email);
     boolean existsByEmail(String email);
+    // 댓글 이름 매핑 최적화 위해
+    List<Member> findAllByEmails(@Param("list") List<String> emails);
+
 }

fb83c9f9cab8f3c3179e3e41c295ed0162320d35
zoey <wjdgustj2877@naver.com>
2025-05-26 13:36:34 +0900

    refactor: 여행 계획 삭제 관련 변경

--- src/main/java/com/ssafy/tripon/plandetail/presentation/request/PlanDetailUpdateRequest.java
+++ src/main/java/com/ssafy/tripon/plandetail/presentation/request/PlanDetailUpdateRequest.java
@@ -3,12 +3,12 @@ package com.ssafy.tripon.plandetail.presentation.request;
 import com.ssafy.tripon.plandetail.application.command.PlanDetailUpdateCommand;
 import java.util.List;
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 
 public record PlanDetailUpdateRequest(
-		@NotBlank
+		@NotNull
 		@Positive
 		Integer day,
 		@Valid
